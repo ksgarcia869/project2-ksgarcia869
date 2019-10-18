21697f266aafffb9c6563eb6d347d95630b48fc7
ksgarcia869 <ksgarcia869@csu.fullerton.edu>
2019-10-17 19:52:33 -0700

    The full code

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -3,15 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <iterator>     // std::next, std::prev
+#include <iterator>
+#include <list>
+    // std::next, std::prev
 
 using std::string;
-
-struct Node{
-    Node *next;
-    Node *prev; 
-    string data; 
-};
 
 class BrowserHistory {
 public:
@@ -28,7 +24,102 @@ public:
     size_t getHistoryLength();
 
 private:
-    Node *head; 
-    Node *tail; 
+    std::list<string> webn;
+    std::list<string> :: iterator n;
+    std::list<string> :: iterator next;
+    std::list<size_t> webs;
+    std::list<size_t> :: iterator s;
   // TO BE COMPLETED
 };
+
+BrowserHistory::BrowserHistory(){
+webn.push_back("");
+webs.push_back(0);
+n = webn.begin();
+s = webs.begin();
+}
+
+void BrowserHistory::visitSite(string url, size_t filesize){
+    size_t counter = 1;
+    size_t holder= webn.size();
+    for(std::list<string> :: iterator i = webn.begin(); *i!=*n;++i){
+      counter++;
+    }
+    if(counter < holder){
+    for(size_t i = counter; i < holder;i++){
+    webn.pop_back();
+    webs.pop_back();
+  }
+  webn.push_back(url);
+  ++n;
+  webs.push_back(filesize);
+  ++s;
+}
+
+  else if(*n==""){
+    webn.push_back(url);
+    ++n;
+    webs.push_back(filesize);
+    ++s;
+    webn.pop_front();
+    webs.pop_front();
+    n = webn.begin();
+    s = webs.begin();
+  }
+  else {
+webn.push_back(url);
+++n;
+webs.push_back(filesize);
+++s;
+}
+}
+
+void BrowserHistory::backButton(){
+
+  if(*n == webn.front()) return;
+  --n;
+  --s;
+}
+
+void BrowserHistory::forwardButton(){
+  if(*n == webn.back()) return;
+  ++n;
+  ++s;
+}
+
+void BrowserHistory::readFile(string fileName){
+std::ifstream myfile(fileName);
+string holder1;
+size_t holder2;
+while(myfile >> holder1){
+  myfile.ignore();
+  if(holder1 =="visit"){
+    myfile >> holder1;
+    myfile.ignore();
+    myfile >> holder2;
+    myfile.ignore();
+    visitSite(holder1, holder2);
+  }
+  else if(holder1=="back"){
+    backButton();
+  }
+  else{
+    forwardButton();
+  }
+}
+}
+
+string BrowserHistory::currentUrl(){
+if(webn.empty()) return "";
+  return *n;
+}
+
+size_t BrowserHistory::currentPageSize(){
+  if(webs.empty()) return 0;
+  return *s;
+}
+
+  size_t BrowserHistory::getHistoryLength(){
+    size_t i = webn.size();
+    return i;
+  }
